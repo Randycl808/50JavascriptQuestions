a83b342aae5cefb2b687b8c3995d9a563de72b1e
Randy Clements <rcxlv@Randys-MacBook-Pro.local>
2022-04-12 18:17:12 -0700

    init

--- 50JavascriptQuestions.js
+++ 50JavascriptQuestions.js
@@ -12,7 +12,7 @@
   let randy = 'Hello my name is Randy Clements'
   // 4)boolean
   let nameFieldChecked = true;
-  let canSing = false;
+  let iCanSing = false;
   // 5)null 
   let age = null;
   // 6)undefined
@@ -27,4 +27,85 @@
 
   // Global-Accessed throughout the entire program
   // Local- Accessed within a block or function 
-  
+
+//Question: 3) What is the difference between "=" "==" and "==="?
+
+  // = (equal to)- sets the variable on left to value on the right
+  favNumber = 5;
+  pi = 3.14;
+  age = 23;
+
+  // == (double equal)- is a comparison operator (requests a boolean)
+  "2" == 2 //true
+  "2" == 1 //false
+
+  // === (triple equal)- checks if two operators are equal (returns a boolean) (always considers operands to be different)
+  1 === 1;               console.log(1 === 1) //true
+  "Hello" === "Hello";   console.log("Hello" === "Hello") //true
+  "Hola" === "Hello";    console.log("Hola" === "Hello") //false
+  "3" === 3;             console.log("3" === 3) //false
+
+//Question: 4) Explain the .pop() and .push() methods
+
+
+//Question: 5) What are higher order functions (HOF)?
+
+
+//Question: 6) Write a loop that prints every number divisible by 5 from 1-200
+
+
+//Question: 7) What is the difference between the "break" and "continue" statements?
+
+
+//Question: 8) What is JSON?
+
+
+//Question: 9) What are arrow functions?
+
+
+//Question: 10) What is TypeScript? How does it differ from Javascript?
+
+
+//Question: 11) What are Promises? Can you use one in an example?
+
+
+//Question: 12) What is async/await?
+
+
+//Question: 13) What is the DOM?
+
+
+//Question: 14) What is a variable?
+
+
+//Question: 15) What is an array?
+
+
+//Question: 16) What is an object?
+
+
+//Question: 17) What is a function?
+
+
+//Question: 18) How would you check if an object has a specific property?
+
+
+//Question: 19) What is the difference between var, const, and let?
+
+
+//Question: 20) How would you check if an array contains a specific item?
+
+
+//Question: 21) How would you check the length of an array?
+
+
+//Question: 22) What is the difference between a for loop and a while loop?
+
+
+//Question: 23) When would you use "&&" and when would you use "||"?
+
+
+//Question: 24) What is a ternary operator?
+
+
+//Question: 25) What is the difference between Java and Javascript?
