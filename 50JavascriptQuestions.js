de7fb6ae3b235a46ae75d4291e7cf8345c9c0a25
Randy Clements <rcxlv@Randys-MacBook-Pro.local>
2022-04-12 17:58:10 -0700

    init

---
+++ 50JavascriptQuestions.js
@@ -0,0 +1,30 @@
+// 50 Javascript Questions
+
+//Question: 1) What are the Javascript data types?
+
+  // 1)number
+  let pi = 3.14;
+  let birthday = 09101998;
+  // 2)bigInt
+  const bigInt = 01234567890123456789n;
+  // 3)string 
+  let string = 'Hello World';
+  let randy = 'Hello my name is Randy Clements'
+  // 4)boolean
+  let nameFieldChecked = true;
+  let canSing = false;
+  // 5)null 
+  let age = null;
+  // 6)undefined
+  let age;
+  console.log(age) // shows undefined
+  // 7)objects 
+  const car = {type:"Ford", model:"Mustang", year:"2018", color:"Black" }
+  const randy ={firstName:"Randy", lastName:"Clements", age:"23", eyeColor:"Blue", dob:"09/10/1998"}
+  // 8)symbol
+
+//Question: 2) What is the difference between local and global variables?
+
+  // Global-Accessed throughout the entire program
+  // Local- Accessed within a block or function 
+  
