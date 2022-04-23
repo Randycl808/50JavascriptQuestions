2d0098de0446191b2697860824be77156477d424
Randy Clements <rcxlv@Randys-MacBook-Pro.local>
2022-04-23 14:24:45 -0700

    init

--- 50JavascriptQuestions.js
+++ 50JavascriptQuestions.js
@@ -141,28 +141,115 @@
 
 //Question: 10) What is TypeScript? How does it differ from Javascript?
 
+  //TypeScript is a superset of the JavaScript language that has a single open-source compiler and is developed mainly by a single vendor: Microsoft. 
+  //The goal of TypeScript is to help catch mistakes early through a type system and to make JavaScript development more efficient.
+  //TypeScript is known as an Object-oriented programming language whereas JavaScript is a scripting language. 
 
 //Question: 11) What are Promises? Can you use one in an example?
 
+  //A promise is an object that may produce a single value some time in the future: either a resolved value, 
+  //or a reason that it’s not resolved (e.g., a network error occurred). 
+  //A promise may be in one of 3 possible states: fulfilled, rejected, or pending. 
+  //Promise users can attach callbacks to handle the fulfilled value or the reason for rejection.
+
+  let done = true
+
+  const isItDoneYet = new Promise((resolve, reject) => {
+    if (done) {
+      const workDone = 'Here is the thing I built'
+      resolve(workDone)
+    } else {
+      const why = 'Still working on something else'
+      reject(why)
+    }
+  })
+
+    //As you can see, the promise checks the done global constant, and if that's true, 
+    //the promise goes to a resolved state (since the resolve callback was called); otherwise, 
+    // the reject callback is executed, putting the promise in a rejected state. 
+    //(If none of these functions is called in the execution path, the promise will remain in a pending state)
 
 //Question: 12) What is async/await?
 
+  //Async/Await is the extension of promises which we get as a support in the language. 
+  //You can refer Promises in Javascript to know more about it.
+
+  //Async:
+  //It simply allows us to write promises based code as if it was synchronous and it checks that we are not breaking the execution thread. It operates asynchronously via the event-loop. 
+  //Async functions will always return a value. It makes sure that a promise is returned and if it is not returned then javascript automatically wraps it in a promise which is resolved with its value.
+
+  //Await:
+  //Await function is used to wait for the promise. It could be used within the async block only. 
+  //It makes the code wait until the promise returns a result. It only makes the async block wait.
+
+  const getData = async() => {
+    var y = await "Data from a website";
+    console.log(y);
+  }
+  
+    console.log(1);
+    getData();
+    console.log(2);  
 
 //Question: 13) What is the DOM?
 
+  //The Document Object Model (DOM) is a programming interface for web documents. 
+  //It represents the page so that programs can change the document structure, style, and content. 
+  //The DOM represents the document as nodes and objects; that way, programming languages can interact with the page.
 
 //Question: 14) What is a variable?
 
+  // Variables are containers for storing data (storing data values).
+  //4 Ways to Declare a JavaScript Variable:
+    //Using var 
+      //When to use var?
+        //The var keyword is used in all JavaScript code from 1995 to 2015.
+        //The let and const keywords were added to JavaScript in 2015.
+        //If you want your code to run in older browser, you must use var.
+    //Using let
+      //When to use let?
+        //If you think the value of the variable can change, use let.
+    //Using const
+      //When to use const?
+        //If you want a general rule: always declare variables with const.
+    //Using nothing
 
 //Question: 15) What is an array?
 
+  //array is a single variable that is used to store different elements. 
+  //It is often used when we want to store list of elements and access them by a single variable. 
+  //Unlike most languages where array is a reference to the multiple variable, in JavaScript array is a single variable that stores multiple elements
+  //An array is a special variable, which can hold more than one value
+
+  const cars = ["Saab", "Volvo", "BMW"];
+    let car = cars[0]; // 0 = "Saab" 1 = "Volvo" 2 = "BMW"
 
 //Question: 16) What is an object?
 
+  //Objects in JavaScript may be defined as an unordered collection of related data, of primitive or reference types, 
+  //in the form of “key: value” pairs. These keys can be variables or functions and are called properties and methods, 
+  //respectively, in the context of an object.
+  //Objects are same as variables in JavaScript, 
+  //the only difference is that an object holds multiple values in terms of properties and methods.
+  //Objects are variables too. But objects can contain many values.
+
+  const car = {type:"Ford", model:"Mustang", year:"2018", color:"Black" }
+  const randy ={firstName:"Randy", lastName:"Clements", age:"23", eyeColor:"Blue", dob:"09/10/1998"}
 
 //Question: 17) What is a function?
 
+  //A JavaScript function is a block of code designed to perform a particular task.
+  //A JavaScript function is executed when "something" invokes it (calls it).
 
+  //Convert Fahrenheit to Celsius function:
+  function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+  }
+
+  //Addition function
+  let sum = add(10, 20);
+    console.log('Sum:', sum);
+    
 //Question: 18) How would you check if an object has a specific property?
 
 
